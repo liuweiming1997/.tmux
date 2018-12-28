e8c8b5d14c8202b4edb213e4057f62413751746a
vimiPonyai <weimingliu@pony.ai>
2018-12-28 18:25:06 +0800

    error code

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 wget https://github.com/libevent/libevent/releases/download/release-2.1.8-stable/libevent-2.1.8-stable.tar.gz
 tar xzvf libevent-2.1.8-stable.tar.gz
 cd libevent-2.1.8-stable
-./configure & make -j8
+./configure && make -j8
 sudo make install
 
 
